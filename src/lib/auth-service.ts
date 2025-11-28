0a2cbcb661e3783b0d579fdddb2fb96ae9bf84b6
LY <ly.jun.ming@gmail.com>
2025-11-28 09:30:37 +0700

    Temp fix on localstorage

--- src/lib/auth-service.ts
+++ src/lib/auth-service.ts
@@ -1,7 +1,7 @@
 
 // src/lib/auth-service.ts
 
-const API_BASE_URL = process.env.NEXT_PUBLIC_API_URL; // Replace with your actual API URL
+const API_BASE_URL = "http://api.fypocr.site"; // Replace with your actual API URL
 
 export type LoginCredentials = {
   username: string; // Backend uses OAuth2PasswordRequestForm which expects 'username'
